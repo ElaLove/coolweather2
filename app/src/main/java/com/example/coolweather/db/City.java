207ac47d088231606449fde53efcd8784a472bb6
Your Name <email@example.com>
2018-06-13 16:50:59 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/example/coolweather/db/City.java
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -1,5 +1,7 @@
 package com.example.coolweather.db;
 
+import org.litepal.crud.DataSupport;
+
 public class City extends DataSupport {
 
     private int id;
--- app/src/main/java/com/example/coolweather/db/County.java
+++ app/src/main/java/com/example/coolweather/db/County.java
@@ -1,5 +1,7 @@
 package com.example.coolweather.db;
 
+import org.litepal.crud.DataSupport;
+
 public class County extends DataSupport {
 
     private int id;
